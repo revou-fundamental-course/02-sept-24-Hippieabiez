ae95cdb5f04c680c3730609ad2fb588bec66f26a
Hippieabiez <kunsumagayi@gmail.com>
2024-09-12 23:52:32 +0700

    konfigurasi maaf kak saya ngestuckk

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // Ini Javascript
+
+
+hitung.style.border = 'none'
+reset.style.border = 'none'
+hitung.style.background = 'lightblue'
+reset.style.background = 'yellow'
+hitung2.style.border = 'none'
+hitung2.style.background = 'lightblue'
+reset2.style.border = 'none'
+reset2.style.background = 'yellow'
+
+function hitungLuasSegitiga(alas,tinggi) {
+    let a = document.getElementById("alas1")
+    let t = document.getElementById("tinggi1")
+    const luas = (0.5 * a * t);
+    console.log('hitung')
+   
+}
